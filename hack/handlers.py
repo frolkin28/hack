e9ae042d7ed9c5fbb3c5168e99f25f89e1870990
Ivan <i.portianyi@smartweb.com.ua>
2022-01-23 00:00:31 +0200

    base handler for sockets

--- hack/handlers.py
+++ hack/handlers.py
@@ -1,5 +1,32 @@
+import logging
+
+import aiohttp
+
 from aiohttp import web
+
+log = logging.getLogger(__name__)
 
 
 async def health_check(request: web.Request) -> web.Response:
     return web.json_response({'status': 'ok'}, status=200)
+
+
+async def websocket_handler(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    async for msg in ws:
+        log.info(f'\n\n\n\n{"#"*100}\n msg {msg}\n{"#"*100}\n\n')
+        if msg.type == aiohttp.WSMsgType.TEXT:
+            if msg.data == 'close':
+                await ws.close()
+            else:
+                await ws.send_str(msg.data + '/answer')
+        elif msg.type == aiohttp.WSMsgType.ERROR:
+            log.error(
+                f'ws connection closed with exception {ws.exception()}'
+            )
+
+    log.info('websocket connection closed')
+
+    return ws
--- hack/routes.py
+++ hack/routes.py
@@ -1,6 +1,6 @@
 from aiohttp import web
 
-from hack.handlers import health_check
+from hack.handlers import health_check, websocket_handler
 
 from hack.views import get_root
 
@@ -9,5 +9,8 @@ def setup_routes(app: web.Application) -> None:
 
     # health check
     app.router.add_get('/health', health_check)
+
+    # web sockets
+    app.router.add_get('/ws', websocket_handler)
 
     app.router.add_get('/', get_root)
