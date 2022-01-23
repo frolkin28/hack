bcc3d000947197ff99f6954334913e68d062daf9
frolkin28 <frolkin2801@gmail.com>
2022-01-23 19:52:59 +0200

    fix url on post request

--- static/api/room.js
+++ static/api/room.js
@@ -1,18 +1,15 @@
-const API_ROUTE = "http://localhost:8080/api"
-
-
 export const postRoom = () =>
-    fetch(`${API_ROUTE}/rooms`, {
+    fetch('/api/rooms', {
         method: 'POST',
         headers: { "Content-Type": "application/json" },
     })
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        return data.room_id;
-    })
-    .catch((err) => {
-        console.log(`Error create room: ${err}`);
-        return ''
-    });
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            return data.room_id;
+        })
+        .catch((err) => {
+            console.log(`Error create room: ${err}`);
+            return ''
+        });
