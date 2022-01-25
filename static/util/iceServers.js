430b3334f1a4f789ab3ecc1477f982d65989c86b
c.pospelova <c.pospelova@smartweb.com.ua>
2022-01-26 00:43:25 +0200

    ice servers

--- static/util/iceServers.js
+++ static/util/iceServers.js
@@ -2,56 +2,56 @@ const getIceServers = () => {
     return {
         'iceServers': [
             {
-                'url': 'stun:stun.l.google.com:19302'
+                'urls': 'stun:stun.l.google.com:19302'
             },
             {
-                'url': 'stun:stun1.l.google.com:19302'
+                'urls': 'stun:stun1.l.google.com:19302'
             },
             {
-                'url': 'stun:stun2.l.google.com:19302'
+                'urls': 'stun:stun2.l.google.com:19302'
             },
             {
-                'url': 'stun:stun3.l.google.com:19302'
+                'urls': 'stun:stun3.l.google.com:19302'
             },
             {
-                'url': 'stun:stun4.l.google.com:19302'
+                'urls': 'stun:stun4.l.google.com:19302'
             },
             {
-                'url': 'stun:stun.ekiga.net'
+                'urls': 'stun:stun.ekiga.net'
             },
             {
-                'url': 'stun:stun.ideasip.com'
+                'urls': 'stun:stun.ideasip.com'
             },
             {
-                'url': 'stun:stun.schlund.de'
+                'urls': 'stun:stun.schlund.de'
             },
             {
-                'url': 'stun:stun.stunprotocol.org:3478'
+                'urls': 'stun:stun.stunprotocol.org:3478'
             },
             {
-                'url': 'stun:stun.voiparound.com'
+                'urls': 'stun:stun.voiparound.com'
             },
             {
-                'url': 'stun:stun.voipbuster.com'
+                'urls': 'stun:stun.voipbuster.com'
             },
             {
-                'url': 'stun:stun.voipstunt.com'
+                'urls': 'stun:stun.voipstunt.com'
             },
             {
-                'url': 'stun:stun.voxgratia.org'
+                'urls': 'stun:stun.voxgratia.org'
             },
             {
-                'url': 'turn:192.158.29.39:3478?transport=udp',
+                'urls': 'turn:192.158.29.39:3478?transport=udp',
                 'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
                 'username': '28224511:1379330808'
             },
             {
-                'url': 'turn:192.158.29.39:3478?transport=tcp',
+                'urls': 'turn:192.158.29.39:3478?transport=tcp',
                 'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
                 'username': '28224511:1379330808'
             },
             {
-                'url': 'turn:numb.viagenie.ca',
+                'urls': 'turn:numb.viagenie.ca',
                 'credential': 'muazkh',
                 'username': 'webrtc@live.com'
             },
